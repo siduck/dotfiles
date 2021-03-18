93435f8b67eaee46e2370b254e2649a85c7fbc45
siduck76 <siduck13@gmail.com>
2021-03-18 15:09:23 +0530

    working lua nvim config

--- nvim_confs/base/nvim/init.lua
+++ nvim_confs/base/nvim/init.lua
@@ -1,41 +1,74 @@
 -- load plugins
-require('pluginsList.lua')
-require('web-devicons.lua')
+require("pluginsList.lua")
+require("web-devicons.lua")
 
-require('utils.lua')
-require('nvimTree.lua')
-require('bufferline.lua')
-require('statusline.lua')
+require("utils.lua")
+require("nvimTree.lua")
+require("bufferline.lua")
+require("statusline.lua")
+require("telescope-nvim.lua")
 
-require('gitsigns.lua')
+-- lsp
+require("nvim-lspconfig.lua")
+require("nvim-compe.lua")
 
-require'colorizer'.setup()
+require("gitsigns.lua")
+
+require "colorizer".setup()
 
 local cmd = vim.cmd
 local g = vim.g
 local indent = 2
 
-cmd 'colorscheme base16-onedark'                      
-cmd 'syntax enable'
-cmd 'syntax on'
+cmd "colorscheme base16-onedark"
+cmd "syntax enable"
+cmd "syntax on"
 
 g.auto_save = 1
-
 g.indentLine_enabled = 1
-g.indentLine_char_list = {'▏'}
+g.indentLine_char_list = {"▏"}
 
-require('treesitter.lua')
-require('mappings.lua')
+g.mapleader = " "
 
--- highlights 
+require("treesitter.lua")
+require("mappings.lua")
+
+-- highlights
 cmd("hi LineNr guibg=NONE")
 cmd("hi SignColumn guibg=NONE")
 cmd("hi VertSplit guibg=NONE")
-cmd("highlight DiffAdd guifg=#81A1C1 guibg = none")
-cmd("highlight DiffChange guifg =#3A3E44 guibg = none")
-cmd("highlight DiffModified guifg = #81A1C1 guibg = none")
+cmd("hi DiffAdd guifg=#81A1C1 guibg = none")
+cmd("hi DiffChange guifg =#3A3E44 guibg = none")
+cmd("hi DiffModified guifg = #81A1C1 guibg = none")
 cmd("hi EndOfBuffer guifg=#282c34")
 
--- tree folder name , icon color 
-cmd("highlight NvimTreeFolderIcon guifg = #61afef")
-cmd("highlight NvimTreeFolderName guifg = #61afef")
+cmd("hi TelescopeBorder   guifg=#3e4451")
+cmd("hi TelescopePromptBorder   guifg=#3e4451")
+cmd("hi TelescopeResultsBorder  guifg=#3e4451")
+cmd("hi TelescopePreviewBorder  guifg=#525865")
+cmd("hi PmenuSel  guibg=#98c379")
+
+-- tree folder name , icon color
+cmd("hi NvimTreeFolderIcon guifg = #61afef")
+cmd("hi NvimTreeFolderName guifg = #61afef")
+cmd("hi NvimTreeIndentMarker guifg=#545862")
+
+require("nvim-autopairs").setup()
+
+require("lspkind").init(
+    {
+        File = " "
+    }
+)
+
+vim.cmd("    hi CustomExplorerBg guibg=#22262C")
+
+vim.api.nvim_exec(
+    [[
+augroup NvimTree 
+  au!
+  au FileType NvimTree setlocal winhighlight=Normal:CustomExplorerBg
+ augroup END
+]],
+    false
+)
