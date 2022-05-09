62b5ce0e1853fa7699813b8986e5223694088e32
siduck <siduck@tutanota.com>
2022-05-09 09:21:03 +0530

    clean nvchad config

--- nvchad/custom/plugins/null-ls.lua
+++ nvchad/custom/plugins/null-ls.lua
@@ -5,7 +5,7 @@ local sources = {
 
    -- webdev stuff
    b.formatting.deno_fmt,
-   b.formatting.prettier.with { filetypes = { "html", "markdown", "css", "astro" } },
+   b.formatting.prettier.with { filetypes = { "html", "markdown", "css" } },
 
    -- Lua
    b.formatting.stylua,
